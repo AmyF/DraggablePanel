2c6a0379ef3409f0d4178fbaf8db9418f53d304d
feida.huang@thoughtworks.com <840382477@qq.com>
2022-10-21 16:43:13 +0800

    Initial Commit

---
+++ DraggableHalfPanel/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  DraggableHalfPanel
+//
+//  Created by Huang Feida on 21/10/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
